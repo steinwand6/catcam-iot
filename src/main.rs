53271890fb07124fa30a1c663868e42ad51ba146
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-06-09 21:07:32 +0900

    feat: Add functiona to capture images via libcamera-jpeg command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+fn main() -> Result<(), ()> {
+    libcam("test.jpeg")
+}
+
+fn libcam(file_name: &str) -> Result<(), ()> {
+    let libcam = Command::new("libcamera-jpeg")
+        .args(["-o", file_name])
+        .output();
+    match libcam {
+        Ok(_) => println!("libcamera-jpeg: {}", file_name),
+        Err(e) => {
+            eprintln!("{:?}", e);
+            return Err(());
+        }
+    }
+    Ok(())
 }
