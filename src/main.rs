2e86c7669fb85e830b33b202a87422f3de8d1049
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-06-10 18:25:00 +0900

    feat: add funfunction to get image bytes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use std::process::Command;
+use std::{fs::File, io::Read, process::Command};
 
 fn main() -> Result<(), ()> {
-    capture_image("test.jpeg")
+    let bytes = get_image_bytes()?;
+    println!("{:?}", bytes);
+    Ok(())
+}
+
+fn get_image_bytes() -> Result<Vec<u8>, ()> {
+    let file_name = "capture.jpeg";
+    capture_image(file_name)?;
+    let mut image_file = match File::open(file_name) {
+        Ok(f) => f,
+        Err(_e) => {
+            return Err(());
+        }
+    };
+    let mut buf = Vec::new();
+    match image_file.read_to_end(&mut buf) {
+        Ok(_) => Ok(buf),
+        Err(_e) => Err(()),
+    }
 }
 
 fn capture_image(file_name: &str) -> Result<(), ()> {
