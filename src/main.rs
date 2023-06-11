0e4268604ea1d33c8446fdae92fcf9400e48c35a
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-06-11 17:51:19 +0900

    feat: add get_options function for customizing libcamera parameters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read, process::Command};
+use std::{env, fs::File, io::Read, process::Command};
 
 fn main() -> Result<(), ()> {
     let bytes = get_image_bytes()?;
@@ -25,6 +25,7 @@ fn get_image_bytes() -> Result<Vec<u8>, ()> {
 fn capture_image(file_name: &str) -> Result<(), ()> {
     let libcam = Command::new("libcamera-jpeg")
         .args(["-o", file_name])
+        .args(get_options())
         .output();
     match libcam {
         Ok(_) => println!("libcamera-jpeg: {}", file_name),
@@ -34,4 +35,26 @@ fn capture_image(file_name: &str) -> Result<(), ()> {
         }
     }
     Ok(())
+}
+
+fn get_options() -> Vec<String> {
+    let ev = env::var("PC_IOT_EV").unwrap_or("0.5".to_string());
+    let shutter = env::var("PC_IOT_SHUTTER").unwrap_or("2000000".to_string());
+    let width = env::var("PC_IOT_WIDTH").unwrap_or("500".to_string());
+    let height = env::var("PC_IOT_HEIGHT").unwrap_or("500".to_string());
+
+    let libcam_args = [
+        "--nopreview",
+        "--ev",
+        &ev,
+        "--shutter",
+        &shutter,
+        "--width",
+        &width,
+        "--height",
+        &height,
+        "--brightness",
+        "0.2",
+    ];
+    libcam_args.map(|e| e.to_string()).to_vec()
 }
