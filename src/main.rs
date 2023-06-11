405a8ebc476e476df9f35f73287123ec3b1554ee
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-06-11 22:06:38 +0900

    feat: add PIR sensor detection for capturing image

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+use rppal::gpio::{Gpio, Trigger};
 use std::error::Error;
 
 mod capture;
 
+const GPIO_NO: u8 = 27;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let bytes = capture::get_image_bytes()?;
-    println!("{:?}", bytes);
+    let mut pir = Gpio::new()?.get(GPIO_NO)?.into_input();
+    pir.set_interrupt(Trigger::RisingEdge)?;
+    loop {
+        match pir.poll_interrupt(true, None) {
+            Ok(_) => {
+                let bytes = capture::get_image_bytes()?;
+                println!("{:?}", bytes);
+            }
+            e => {
+                eprint!("{:?}", e);
+                break;
+            }
+        }
+    }
+
     Ok(())
 }
