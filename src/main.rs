b10b452dfaa092ea30ae9422620997f4f210c9b4
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-06-14 23:06:57 +0900

    feat: add initial MQTT communication to AWS IoT

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rppal::gpio::{Gpio, Trigger};
-use std::error::Error;
+use rumqtt::{MqttClient, MqttOptions, QoS};
+use std::{env, error::Error};
 
 mod capture;
 
@@ -8,11 +9,22 @@ const GPIO_NO: u8 = 27;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut pir = Gpio::new()?.get(GPIO_NO)?.into_input();
     pir.set_interrupt(Trigger::RisingEdge)?;
+
+    let mqtt_options = get_mqtt_options();
+    let (mut mqtt_client, _notifications) = MqttClient::start(mqtt_options).unwrap();
+
     loop {
         match pir.poll_interrupt(true, None) {
             Ok(_) => {
                 let bytes = capture::get_image_bytes()?;
                 println!("{:?}", bytes);
+
+                let payload = "hello AWS IoT!";
+                let qos = QoS::AtLeastOnce;
+                let retain = false;
+                mqtt_client
+                    .publish("cat-camera/capture", qos, retain, payload)
+                    .unwrap();
             }
             e => {
                 eprint!("{:?}", e);
@@ -22,4 +34,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+fn get_mqtt_options() -> MqttOptions {
+    let ca = include_bytes!("../cert/AmazonRootCA1.pem").to_vec();
+    let client_crt = include_bytes!("../cert/cat-camera.crt").to_vec();
+    let client_key = include_bytes!("../cert/private.key").to_vec();
+    let mqtt_host = "cat-camera";
+    let endpoint = env::var("AWS_IOT_ENDPOINT").unwrap();
+
+    MqttOptions::new(mqtt_host, endpoint, 8883)
+        .set_ca(ca)
+        .set_client_auth(client_crt, client_key)
+        .set_keep_alive(10)
+        .set_reconnect_opts(rumqtt::ReconnectOptions::Always(5))
 }
